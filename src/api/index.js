135eeeb5b775cdae9dd6dfa11ff8cfd1b73b893c
Kunal Shah <kunalshah1996@gmail.com>
2022-11-22 00:01:05 -0600

    Made changes for dev env

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://appbuddy.onrender.com/', withCredentials: true }); //'http://localhost:8000/'
+const API = axios.create({ baseURL: 'http://localhost:8000/', withCredentials: true }); //'https://appbuddy.onrender.com/'
 
 
 
