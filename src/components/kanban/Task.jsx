c2c64a70119e4ed619a64cb9b5e67652cfa6ac7d
SamruddhiKalsekar <samruddhikalsekar3@gmail.com>
2022-12-02 02:29:17 -0600

    Merged dev to main

--- src/components/kanban/Task.jsx
+++ src/components/kanban/Task.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Draggable } from "react-beautiful-dnd";
+import ReactModal from "react-modal";
 
 import { API } from "../../api/index";
 
 const Container = styled.div`
+display: flex;
+justify-content: space-between;
   border: 1px solid black;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
   background-color: white;
 `;
+
+ReactModal.setAppElement("#root");
 
 const Task = (props) => {
   console.log(props.task);
@@ -44,25 +49,55 @@ const Task = (props) => {
       });
   };
 
+  const [showModal, setShowModal] = useState(false);
+  const [modalData, setModalData] = useState({});
+
+  const handleOpenModal = (task) => {
+    console.log(task);
+    setShowModal(true);
+    setModalData(task);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setModalData({});
+  };
+
   return (
-    <Draggable draggableId={`${props.task.id}`} index={props.index}>
-      {(provided) => (
-        <Container
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-        >
-          {props.task.company_name}
-          <span
-            onClick={() => {
-              deleteTask(props.columnId, props.index, props.task.id);
-            }}
+    <>
+      <Draggable draggableId={`${props.task.id}`} index={props.index}>
+        {(provided) => (
+          <Container
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            onClick={() => handleOpenModal(props.task)}
           >
-            X
-          </span>
-        </Container>
-      )}
-    </Draggable>
+            {props.task.company_name}
+            <span style={{marginLeft:"15px"}}
+              onClick={() => {
+                deleteTask(props.columnId, props.index, props.task.id);
+              }}
+            >
+              X
+            </span>
+          </Container>
+        )}
+      </Draggable>
+      <ReactModal isOpen={showModal}>
+        <button onClick={handleCloseModal}>Close</button>
+        <h1> Company Name : {modalData.company_name}</h1>
+        <h2>Position : {modalData.position}</h2>
+        {modalData.oa_link ? (
+          <div>
+            <h2>OA Email Link : <a href={modalData.oa_link} target="_blank">Click to open email</a></h2>
+            <h2>Deadline : {modalData.deadline}</h2>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </ReactModal>
+    </>
   );
 };
 
